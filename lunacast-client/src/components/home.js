299c6c60ccf7480b7b57970f2f907f7819fc23b7
“Tinisi” <jgrosz@tinisi.com>
2017-04-02 01:00:43 -0500

    adding style sheet and fixing camel case attrs,

--- lunacast-client/src/components/home.js
+++ lunacast-client/src/components/home.js
@@ -7,7 +7,7 @@ class Home extends Component {
       <div className="home">
         <div className="home-header">
           <h2>Welcome!</h2>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/1wQRxH0jRyI?autoplay=1" frameborder="0" allowfullscreen></iframe>
+          <iframe className="video" src="https://www.youtube.com/embed/1wQRxH0jRyI?autoplay=1" allowFullScreen="allowFullScreen"></iframe>
         </div>
       </div>
     );
